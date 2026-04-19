3b43cf673e483a3d48360f29656735d2b3f23569
kaif0008 <mrkaif7295@gmail.com>
2026-04-19 16:26:59 +0530

    Deployment readyr

--- public/js/config.js
+++ public/js/config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     // Example: "https://nextrade-api.onrender.com"
     API_BASE_URL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' 
         ? 'http://localhost:5010' 
-        : 'https://nextrade-0-2.onrender.com', // Change this to your Render URL
+        : 'https://nextrade-backend-0-2.onrender.com', // Change this to your Render URL
 };
 
 // Log configuration status
